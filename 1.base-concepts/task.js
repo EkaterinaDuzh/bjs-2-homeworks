11d37696360753f85409b9bf1f90f953f854d6ce
EkaterinaDuzh <ekaterinaduzh@gmail.com>
2024-07-24 23:28:26 +0300

    Задание 1-2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,18 +1,34 @@
 "use strict"
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-	let monthPercent = percent / 100 / 12;
-	let contribution;
-	let amount;
-	let countMonths;
-	let totalAmount;
-	let month;
-	let bodyCredit;
-	if (!isNaN(percent) && !isNaN(contribution) && !isNaN(amount)) {
-		const bodyCredit = amount - contribution;
-		const month = bodyCredit * (monthPercent + (monthPercent / (((1 + monthPercent) ** countMonths) - 1)));
-		const totalAmount = parseFloat((month * countMonths).toFixed(2));
-		return totalAmount;
-	}
-	return false;
+function solveEquation(a, b, c) {
+  let arr = [];
+  let x1;
+  let x2;
+  let d = b ** 2 -4 * a * c;
+
+  if (d > 0) {
+    x1 = (-b + Math.sqrt(d)) / (2 * a);
+    x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1, x2);
+  } else if (d === 0) {
+    x1 = -b / (2 * a);
+    arr.push(x1);
+  }
+  return arr;
 }
+
+  function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+
+    let monthPercent = percent / 100 / 12;
+    let totalAmount;
+    let month;
+    let bodyCredit;
+
+
+    if (!isNaN(percent) && !isNaN(contribution) && !isNaN(amount)) {
+      const bodyCredit = amount - contribution;
+      return parseFloat(((bodyCredit * (monthPercent + (monthPercent / (((1 + monthPercent) ** countMonths) - 1)))) * countMonths).toFixed(2));
+    }
+    return false;
+  }
+  
